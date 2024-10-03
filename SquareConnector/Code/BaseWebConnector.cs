e40a90863c1482ac8f37f87064473a51adeb7f84
blackwinged <lovro.gamulin@gmail.com>
2024-10-03 23:21:25 +0200

    pocetak konektora

---
+++ SquareConnector/Code/BaseWebConnector.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace SquareConnector.Code;
+
+public abstract class BaseWebConnector
+{
+
+    public abstract List<string> GrabTargetLinks();
+    // public abstract
+}
---
+++ SquareConnector/Code/VincekConnector/VincekConnector.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace SquareConnector.Code.VincekConnector;
+
+public class VincekConnector
+{
+    public Dictionary<string, string> TargetLinks {get; set;} = new Dictionary<string, string>();
+    public string ProductPath {get; set;} = "";
+}
