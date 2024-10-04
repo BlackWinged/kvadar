f1bd233d36ed72b293ce008aec4352e3a76ca310
lovro <lovro.gamulin@gmail.com>
2024-10-04 19:06:55 +0200

    dicking around with tests

--- tests/elmatis/tests/elmatis_test.spec.ts
+++ tests/elmatis/tests/elmatis_test.spec.ts
@@ -1,23 +1,17 @@
 import { test, expect, chromium } from '@playwright/test';
 
+const NavigationLinkDirectory = {};
+
 test('check all links', async ({ page }) => {
   await page.goto('https://elmatis.hr/');
   // Get all the links on the page
-  const links = await page.$$eval('a', anchors => anchors.map(anchor => anchor.href));
-  
 
-  for (const link of links) {
-    try {
-        console.log(`Opening URL: ${link}`);
-       
-        await page.goto(link);
-        // You can perform additional actions here if needed
+  const loc = await page.locator('a');
+  const links = await loc.evaluateAll(anchors => anchors.map(x => x.getAttribute("href")));
 
-    } catch (error) {
-        console.error(`Failed to open URL: ${link}`, error);
-    }
-}
-  
+
+  await GrabAllLinks(links, page);
+
 });
 
 test('get started link', async ({ page }) => {
@@ -29,3 +23,28 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+
+async function GrabAllLinks(startingLinks, page) {
+  for (const link of startingLinks) {
+    if (!(await link in NavigationLinkDirectory)) {
+      try {
+        console.log(`Opening URL: ${link}`);
+        
+        const pageLoc = await page.locator("[href='"+ link + "']");
+
+        await pageLoc.click();
+        const loc = await page.locator('a');
+        const links = await loc.evaluateAll(anchors => anchors.map(x => x.getAttribute("href")));
+
+        const filteredLinks = links.filter(x => x in NavigationLinkDirectory);
+        NavigationLinkDirectory[link.href] = 1;
+        if (filteredLinks.len > 0) {
+          GrabAllLinks(filteredLinks, page);
+        }
+      } catch (error) {
+        console.error(`Failed to open URL: ${link.href}`, error);
+      }
+    }
+  }
+}
